c4c4e97f00fd693c1f98f51159dcb27154d6fb4b
Matheus <matrodrigues@live.com>
2022-04-21 19:49:38 -0300

    Implement eraser button, change number of squares input type and end stylization

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const colorOptions = document.getElementById('colorOptions');
 const colorPicker = document.getElementById('colorPicker');
 const colorSubmit = document.getElementById('colorSubmit');
 const numberSquares = document.getElementById('numberSquares');
+const sizeInfo = document.getElementById('sizeInfo');
+const eraserSelect = document.getElementById('eraserSelect');
 
 const blackColorSelector = document.querySelector('input[type=checkbox]');
 blackColorSelector.addEventListener('change', () => {
@@ -20,37 +22,35 @@ blackColorSelector.addEventListener('change', () => {
       
         console.log("Checkbox is checked..");
     } else {
-        colorOptions.classList.remove('show');
         resetSketch(numSquares); 
         generateHoverEffect(squaresArray,"rgb(56, 54, 54)");
         console.log("Checkbox is not checked..");
+        colorOptions.classList.remove('show');
+        colorPicker.classList.remove('show');
     }
+    eraserSelect.classList.remove('eraser-clicked');
   });
 
+eraserSelect.addEventListener('click', handleForm);
 
+eraserSelect.addEventListener('click', startEraser)
+
+function startEraser () {
+    if(eraserSelect.classList.contains('eraser-clicked')){
+        eraserSelect.classList.remove('eraser-clicked');
+        if(blackColorSelector.checked){
+            generateHoverEffect(squaresArray,"rainbow");   
+        } else {
+            generateHoverEffect(squaresArray,"rgb(56, 54, 54)"); 
+        }
+    } else{
+        eraserSelect.classList.add('eraser-clicked');
+        generateHoverEffect(squaresArray,"white");
+    }
+}
 
 
   colorOptions.addEventListener('submit', getColorOption);
-
-
-// colorOptions.addEventListener('submit', () => {
-//     colorOptionSelected = document.querySelector('input[name="color"]:checked').value;
-//     if(colorOptionSelected == 'pickOne'){
-//         //colorPicker.classList.toggle('show');
-//         //colorOptions.style.display = 'none';
-//         getNewColor();
-//         resetSketch(numSquares);
-//         console.log(colorSubmitValue);
-//     } else if (colorOptionSelected =='randomColor'){
-//         randColor = `rgb(${Math.floor(Math.random()*255 +1)},${Math.floor(Math.random()*255 +1)},${Math.floor(Math.random()*255 +1)})`;
-//         resetSketch(numSquares);
-//         generateHoverEffect(squaresArray,randColor);
-//         console.log(randColor); 
-//     } else {
-//         resetSketch(numSquares);
-//     }   
-
-// });
 
 function getColorOption(){
     colorOptionSelected = document.querySelector('input[name="color"]:checked').value;
@@ -64,10 +64,12 @@ function getColorOption(){
         randColor = `rgb(${Math.floor(Math.random()*255 +1)},${Math.floor(Math.random()*255 +1)},${Math.floor(Math.random()*255 +1)})`;
         resetSketch(numSquares);
         generateHoverEffect(squaresArray,randColor);
+        colorPicker.classList.remove('show');
         console.log(randColor); 
     } else {
         resetSketch(numSquares);
         generateHoverEffect(squaresArray,"rainbow");
+        colorPicker.classList.remove('show');
     }
     return colorOptionSelected;   
 }
@@ -89,13 +91,19 @@ function getNewColor(){
     colorPicker.classList.toggle('show');
 }
 
-numberSquares.addEventListener('submit', () => {
+sizeInfo.textContent = `${numSquares}x${numSquares}`
+
+numberSquares.addEventListener('change', () => {
     numSquaresSelected = document.getElementById('numberOfSquares').value;
     if (numSquaresSelected){
         numSquares = numSquaresSelected;
     } 
-        resetButtonHandle(numSquares);
-        numberSquares.classList.remove('show');
+    sizeInfo.textContent = `${numSquaresSelected}x${numSquaresSelected}`    
+    resetSketch(numSquares);
+    if(blackColorSelector.checked){
+        generateHoverEffect(squaresArray,"rainbow"); 
+    }
+        
 });
 
 
@@ -172,7 +180,8 @@ function resetButtonHandle(){
     };
 
 
-
+    squaresArray.forEach(square => square.addEventListener('click', () => {
+        square.style.backgroundColor = 'white';}));
 
 
 
